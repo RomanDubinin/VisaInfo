6fb098f629b329a5f0f252f10a65d782331f7f51
RomanDubinin <romandubinin94@gmail.com>
2020-01-29 10:05:28 +0500

    StatementNumberGenerator

---
+++ VisaProject/DataLoad/StatementNumberGenerator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DataLoad
+{
+    public class StatementNumberGenerator
+    {
+        public string Generate(string city, DateTime date, int number)
+        {
+            return $"{city}{date:yyyMMdd}{number.ToString().PadLeft(4, '0')}";
+        }
+    }
+}
---
+++ VisaProject/UnitTests/StatementNumberGeneratorTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using DataLoad;
+using NUnit.Framework;
+
+namespace UnitTests
+{
+    public class StatementNumberGeneratorTests
+    {
+        private StatementNumberGenerator StatementNumberGenerator { get; set; }
+
+        [SetUp]
+        public void Setup()
+        {
+            StatementNumberGenerator = new StatementNumberGenerator();
+        }
+
+        [Test]
+        public void TestCreateFromStringWith3PaddingZeros()
+        {
+            var city = "YEKA";
+            var date = new DateTime(2017, 04, 23);
+            var number = 1;
+            var expectedStatementNumber = "YEKA201704230001";
+
+            var actualStatementNumber = StatementNumberGenerator.Generate(city, date, number);
+
+            Assert.That(actualStatementNumber, Is.EqualTo(expectedStatementNumber));
+        }
+
+        [Test]
+        public void TestCreateFromStringWith1PaddingZero()
+        {
+            var city = "YEKA";
+            var date = new DateTime(2017, 04, 23);
+            var number = 123;
+            var expectedStatementNumber = "YEKA201704230123";
+
+            var actualStatementNumber = StatementNumberGenerator.Generate(city, date, number);
+
+            Assert.That(actualStatementNumber, Is.EqualTo(expectedStatementNumber));
+        }
+
+    }
+}
