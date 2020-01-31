9eaf0412ee4eed6bc7a22bd4843bc42740fa5b5e
RomanDubinin <romandubinin94@gmail.com>
2020-01-31 09:52:24 +0500

    web service

---
+++ VisaProject/WebVisaProject/Controllers/VisaInfoController.cs
@@ -0,0 +1,35 @@
+﻿using DataContainer;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using VisaProject;
+
+namespace WebVisaProject.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class VisaInfoController : ControllerBase
+    {
+
+        private readonly ILogger<VisaInfoController> _logger;
+        private readonly IRepository repository;
+        private readonly StatisticBuilder statisticBuilder;
+
+        public VisaInfoController(
+            ILogger<VisaInfoController> logger,
+            IRepository repository,
+            StatisticBuilder statisticBuilder)
+        {
+            _logger = logger;
+            this.repository = repository;
+            this.statisticBuilder = statisticBuilder;
+        }
+
+        [HttpGet]
+        public VisaStatisticItem[] Get(string city)
+        {
+            var infos = repository.ReadAll();
+            var statistic = statisticBuilder.BuildStatisticByDays(infos);
+            return statistic;
+        }
+    }
+}
