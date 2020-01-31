79da0263b814897d80f213af9d0a6267ab48bdc9
RomanDubinin <romandubinin94@gmail.com>
2020-01-31 20:09:34 +0500

    to private fields in tests

--- VisaProject/UnitTests/DataUpdaterTests.cs
+++ VisaProject/UnitTests/DataUpdaterTests.cs
@@ -10,22 +10,22 @@ namespace UnitTests
 {
     public class DataUpdaterTests
     {
-        public IRepository Repository;
-        public Mock<IVisaResultLoader> VisaResultLoader;
-        public DataUpdater DataUpdater;
-        public StatementNumberGenerator StatementNumberGenerator;
-        public readonly string City = "CITY";
-        public readonly VisaInfoFilter EmptyFilter = new VisaInfoFilter();
+        private IRepository repository;
+        private Mock<IVisaResultLoader> visaResultLoader;
+        private DataUpdater dataUpdater;
+        private StatementNumberGenerator statementNumberGenerator;
+        private readonly string city = "CITY";
+        private readonly VisaInfoFilter emptyFilter = new VisaInfoFilter();
 
         [SetUp]
         public void Setup()
         {
-            Repository = new TestRepository();
-            VisaResultLoader = new Mock<IVisaResultLoader>();
-            VisaResultLoader.Setup(x => x.LoadVisaResultByStatementNumber(It.IsAny<string>()))
+            repository = new TestRepository();
+            visaResultLoader = new Mock<IVisaResultLoader>();
+            visaResultLoader.Setup(x => x.LoadVisaResultByStatementNumber(It.IsAny<string>()))
                             .Returns(Task.FromResult(VisaResult.InService));
-            StatementNumberGenerator = new StatementNumberGenerator();
-            DataUpdater = new DataUpdater(StatementNumberGenerator, VisaResultLoader.Object, Repository);
+            statementNumberGenerator = new StatementNumberGenerator();
+            dataUpdater = new DataUpdater(statementNumberGenerator, visaResultLoader.Object, repository);
         }
 
         [Test]
@@ -36,14 +36,14 @@ namespace UnitTests
 
             var expectedData = new[]
             {
-                new VisaInfo(City, VisaResult.InService, "CITY202004010001", new DateTime(2020, 4, 1)),
-                new VisaInfo(City, VisaResult.InService, "CITY202004010002", new DateTime(2020, 4, 1)),
-                new VisaInfo(City, VisaResult.InService, "CITY202004010003", new DateTime(2020, 4, 1)),
+                new VisaInfo(city, VisaResult.InService, "CITY202004010001", new DateTime(2020, 4, 1)),
+                new VisaInfo(city, VisaResult.InService, "CITY202004010002", new DateTime(2020, 4, 1)),
+                new VisaInfo(city, VisaResult.InService, "CITY202004010003", new DateTime(2020, 4, 1)),
             };
 
-            DataUpdater.UpdateData(dateFrom, dateTo, City, 3, dayOfWeek => true, () => { });
+            dataUpdater.UpdateData(dateFrom, dateTo, city, 3, dayOfWeek => true, () => { });
 
-            var actualData = Repository.Read(EmptyFilter);
+            var actualData = repository.Read(emptyFilter);
 
             Assert.That(actualData, Is.EqualTo(expectedData));
         }
@@ -59,14 +59,14 @@ namespace UnitTests
 
             var expectedData = new[]
             {
-                new VisaInfo(City, VisaResult.InService, "CITY202004030001", new DateTime(2020, 4, 3)),
-                new VisaInfo(City, VisaResult.InService, "CITY202004050001", new DateTime(2020, 4, 5)),
-                new VisaInfo(City, VisaResult.InService, "CITY202004070001", new DateTime(2020, 4, 7)),
+                new VisaInfo(city, VisaResult.InService, "CITY202004030001", new DateTime(2020, 4, 3)),
+                new VisaInfo(city, VisaResult.InService, "CITY202004050001", new DateTime(2020, 4, 5)),
+                new VisaInfo(city, VisaResult.InService, "CITY202004070001", new DateTime(2020, 4, 7)),
             };
 
-            DataUpdater.UpdateData(dateFrom, dateTo, City, 1, IsWorkingDay, () => { });
+            dataUpdater.UpdateData(dateFrom, dateTo, city, 1, IsWorkingDay, () => { });
 
-            var actualData = Repository.Read(EmptyFilter);
+            var actualData = repository.Read(emptyFilter);
 
             Assert.That(actualData, Is.EqualTo(expectedData));
         }
