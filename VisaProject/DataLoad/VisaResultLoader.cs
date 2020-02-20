61d09e1368300dbb1fe769633b6dbf3df51d5a30
RomanDubinin <romandubinin94@gmail.com>
2020-02-20 10:04:18 +0500

    add retry

--- VisaProject/DataLoad/VisaResultLoader.cs
+++ VisaProject/DataLoad/VisaResultLoader.cs
@@ -1,6 +1,8 @@
-﻿using System.Net.Http;
+﻿using System;
+using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using VisaProject;
 
@@ -8,6 +10,7 @@ namespace DataLoad
 {
     public class VisaResultLoader : IVisaResultLoader
     {
+        private readonly int retryCount = 5;
         private readonly HttpClient client = new HttpClient();
         private readonly VisaInfoParser visaInfoParser = new VisaInfoParser();
 
@@ -21,11 +24,27 @@ namespace DataLoad
         {
             var content = new StringContent($"ioff_zov={statementNumber}&op=Ov%C4%9B%C5%99it&form_id=ioff_application_status_form", Encoding.UTF8);
             content.Headers.ContentType = new MediaTypeHeaderValue("application/x-www-form-urlencoded");
-            var response = await client.PostAsync("https://frs.gov.cz/cs/ioff/application-status", content);
 
-            var responseString = await response.Content.ReadAsStringAsync();
+            for (int i = 1; i <= retryCount; i++)
+            {
+                try
+                {
+                    var response = await client.PostAsync("https://frs.gov.cz/cs/ioff/application-status", content);
 
-            return visaInfoParser.Parse(responseString);
+                    var responseString = await response.Content.ReadAsStringAsync();
+
+                    return visaInfoParser.Parse(responseString);
+                }
+                catch (Exception e)
+                {
+                    if (i == retryCount)
+                    {
+                        throw;
+                    }
+                    Thread.Sleep(TimeSpan.FromSeconds(5));
+                }
+            }
+            throw new Exception("Must never be thrown");
         }
     }
 }
